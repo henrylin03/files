08085f933fccc607fd9946f420482d700404f217
Henry <83106787+henrylin03@users.noreply.github.com>
2026-02-22 10:55:35 +1100

    feat: setup prisma for postgresql, including defining schema models and initialising client (#1)
    * chore: install required dependencies
    
    these are required by the 'quickstart' docs in prisma ORM for postgresql db
    
    * feat: generate prisma schema
    
    * feat: add User model to schema
    
    using uuid because i think it's safer than just using increments. followed docs from prisma here: https://www.prisma.io/docs/orm/prisma-schema/data-model/unsupported-database-features#native-database-functions to call uuid() within @default. autoincrement would perform better, but i feel like i should follow best practices
    
    * chore: delete db folder
    
    these aren't used anymore as we're going with prisma orm!
    
    * docs(schema): add rough schema design
    
    * feat(schema): build schemas based on my understanding of my schema-plan
    
    * feat(migrations): initialise sql tables using prisma migrate
    
    * style: include prisma and sql files for styling
    
    * feat(prisma.ts): instantiate prisma client
    
    * feat(schema): add password field to user, and default the uploaded time to 'now'. migrate.
    
    password field missing was an oversight
    
    * feat(script.ts): add a single user
    
    there were errors when trying to add files and folders associated with user that i didn't understand, so i just created a user to keep it minimal and have now confirmed that prisma is set up sufficiently properly (initial aim of this script.ts in quick-start docs iiuc). can fix issues in the future in another pr when i start building particular features
    
    * fix(lefthook.yml): remove sql and prisma files from biome styling
    
    was throwing error 'processing panicked: byte index 1 is out of bounds of '''
    
    * chore(script): remove
    
    not really necessary to seed the db at the moment

---
+++ prisma/migrations/20260221100647_init/migration.sql
@@ -0,0 +1,50 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "sizeInKb" INTEGER NOT NULL,
+    "fileExtension" TEXT NOT NULL,
+    "uploadedDateTime" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "folderId" INTEGER NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "File_name_key" ON "File"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Folder_name_key" ON "Folder"("name");
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
