07a9160cca60859d58e4e45e55fb62f0d51c7323
Henry <83106787+henrylin03@users.noreply.github.com>
2026-02-21 12:55:33 +1100

    batman

---
+++ README.md
@@ -0,0 +1,2 @@
+# files
+Full-stack TypeScript web app, built in Express.js, that allows users to upload and share files with the world.
---
+++ src/db/queries.ts
@@ -0,0 +1,13 @@
+// import { Pool } from "pg";
+import "dotenv/config";
+
+/* WRITE QUERIES HERE */
+
+// async function getAllMessages() {
+//   const { rows } = await pool.query("SELECT * FROM messages");
+//   return rows;
+// }
+
+// module.exports = {
+//   getAllMessages,
+// };
