64bc62b4c4bc5f1a55a5024cffaacd6f357bb526
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-23 15:31:51 +1100

    feat(signup): if user inputs failed server-side validation, still keep the data that the users have inputted last time, except the passwords
    this saves them from having to retype them all out again. however, based on patterns that ive observed on other websites, we do not restore the passwords -- users should retype the passwords all over again (including the confirmation of it)

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -11,12 +11,21 @@ const signupGet = async (_req: Request, res: Response) => {
 const signupPost = [
 	validateSignupForm,
 	async (req: Request, res: Response) => {
+		const formData = matchedData(req, {
+			onlyValidData: false,
+		});
+		const { firstName, lastName, username } = formData;
+
 		const errors = validationResult(req);
 		if (!errors.isEmpty())
-			return res.status(400).render("pages/signup", { errors: errors.array() });
+			return res.status(400).render("pages/signup", {
+				errors: errors.array(),
+				firstName,
+				lastName,
+				username,
+			});
 
-		const { firstName, lastName, username, password } = matchedData(req);
-
+		const { password } = formData;
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(password, salt);
 
