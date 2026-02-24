8d21622bbfeb052833f566c2dfc09c14960597c0
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-24 14:53:24 +1100

    feat: by default, redirect user to the login page

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -3,7 +3,7 @@ import type { Request, Response } from "express";
 const homePageGet = (_req: Request, res: Response) => {
 	// TODO: if user is not signed in yet, take user to login page
 	// TODO: if user _is_ signed in, then take them to their dashboard
-	res.render("pages/index");
+	res.redirect("/login");
 };
 
 export { homePageGet };
