2f8d2d7144c26a6b11d6d893d4ee569add5a5d0a
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-25 11:33:11 +1100

    refactor(multer): use the storage config for multer instance to define filename

--- src/lib/multer.ts
+++ src/lib/multer.ts
@@ -1,5 +1,13 @@
 import multer from "multer";
 
-const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+	destination: "./uploads",
+	filename: (_req, file, cb) => {
+		const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+		cb(null, `${file.originalname}-${uniqueSuffix}`);
+	},
+});
+
+const upload = multer({ storage });
 
 export { upload };
