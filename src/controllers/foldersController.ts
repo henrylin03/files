eb3a725c52278344e395bb65aa661d0126a05e1d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-25 13:07:05 +1100

    feat(newFolder.eta): user is able to attempt to add new folder, with inputted folder name. this is server-side-validated using express-validator.
    if fails, displayed in error element (errorList.eta). validation includes checking for special characters (only hyphens, underscores, spaces, numbers and letters allowed), length (minimum 1 char, max 255 char, following Google Drive limitations on the web)
    
    Squashed commit of the following:
    
    commit 0e0b0136391fb398e09ba3ee9781e5911f1fd78f
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Wed Feb 25 12:59:20 2026 +1100
    
        feat(newFolder.eta): add placeholder text in input to be more informative
    
    commit 8181fcfdd2beed04355c2ded805578fbc6a739bd
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Wed Feb 25 12:56:24 2026 +1100
    
        feat(newFolder.eta): display server-side validation errors
    
    commit 87e9bd31b5576589827b7215fca60a69b710b9d8
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Wed Feb 25 12:52:40 2026 +1100
    
        feat(validation): folder names cannot have any special characters, other than hyphens and underscores
    
        i was thinking that the alternative would be to allow it, but _escape_ it, but imo just cleaner for user to just not allow them to use those special characters. because, if we allow it and escape it, they'll see a bunch of symbols in their dashboard
    
    commit b9653a1a7c368455af2559e65f746ffe445adf15
    Merge: ddedc57 d011557
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Wed Feb 25 12:40:39 2026 +1100
    
        Merge branch 'feat/enable-users-to-add-folders-then-display-them-on-dashboard' into feat-add-validation-for-adding-new-folder
    
    commit ddedc57faa8e451b769a69f64e1341e098ff97cd
    Author: Henry Lin <83106787+henrylin03@users.noreply.github.com>
    Date:   Wed Feb 25 12:36:48 2026 +1100
    
        feat(validation): add validation for new folder
    
        right now there is only one field for it, and that's the folder's name. we check to see if it isn't empty, and then have it have max 255 characters, which aligns with Google Drive's folder requirements on the web

--- src/controllers/foldersController.ts
+++ src/controllers/foldersController.ts
@@ -1,17 +1,32 @@
 import type { Request, Response } from "express";
+import { matchedData, validationResult } from "express-validator";
+import { validateNewFolder } from "@/lib/validationUtils.js";
+
+const PAGE_TITLE = "Add folder";
 
 const foldersGet = async (_req: Request, res: Response) => {
 	res.redirect("/");
 };
 
 const addFolderGet = async (_req: Request, res: Response) => {
-	res.render("pages/newFolder", { title: "Add folder" });
+	res.render("pages/newFolder", { title: PAGE_TITLE });
 };
 
-const addFolderPost = async (req: Request, res: Response) => {
-	const { body } = req;
-	console.log(body);
-	res.redirect("/folders");
-};
+const addFolderPost = [
+	validateNewFolder,
+	async (req: Request, res: Response) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty())
+			return res.status(400).render("pages/newFolder", {
+				title: PAGE_TITLE,
+				errors: errors.array(),
+			});
+
+		const { folderName } = matchedData(req);
+		console.log(folderName);
+
+		res.redirect("/folders");
+	},
+];
 
 export { addFolderGet, addFolderPost, foldersGet };
--- src/lib/validationUtils.ts
+++ src/lib/validationUtils.ts
@@ -50,4 +50,17 @@ const validateSignupForm = [
 		.withMessage("Passwords must match"),
 ];
 
-export { validateSignupForm };
+const validateNewFolder = [
+	body("folderName")
+		.trim()
+		.notEmpty()
+		.withMessage("Folder name is required")
+		.isAlphanumeric("en-AU", { ignore: "-_ " })
+		.withMessage(
+			"Folder name can only letters, numbers, spaces, hyphens and underscores",
+		)
+		.isLength({ min: 1, max: 255 })
+		.withMessage("Please enter a folder name that is max 255 characters"),
+];
+
+export { validateSignupForm, validateNewFolder };
