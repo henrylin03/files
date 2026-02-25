c93589d243a317de18883d018b5ee9ab453d2eac
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-25 12:40:10 +1100

    feat(validation): signup form's fields are validated to check if empty first, before going through subsequent checks
    username check in db is delayed to the very end, as we don't want to do this until the low-hanging-fruit checks (like character limits) are done

--- src/lib/validationUtils.ts
+++ src/lib/validationUtils.ts
@@ -20,19 +20,25 @@ const validateSignupForm = [
 
 	body("username")
 		.trim()
+		.notEmpty()
+		.withMessage(
+			"Username is a required field. Please enter your email address.",
+		)
+		.isEmail()
+		.withMessage("Please enter a valid email address")
+		.isLength({ max: 256 })
+		.withMessage("Please enter an email address that is max 256 characters")
 		.custom(async (inputtedUsername: string) => {
 			const user = await prisma.user.findUnique({
 				where: { username: inputtedUsername },
 			});
 			if (user)
 				throw new Error("Username is already in use. Please log in instead.");
-		})
-		.isEmail()
-		.withMessage("Please enter a valid email address")
-		.isLength({ max: 256 })
-		.withMessage("Please enter an email address that is max 256 characters"),
+		}),
 
 	body("password")
+		.notEmpty()
+		.withMessage("Please set a password")
 		.isLength({ min: 8, max: 64 })
 		.withMessage("Password must be between 8 and 64 characters"),
 
