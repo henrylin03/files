e0452335718cfe973d3a37b5a9cdcb76523980a9
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2026-02-25 11:43:45 +1100

    feat(multer): ensure file name, once uploaded, is unique
    this is done by grabbing the file.originalname and doing some string manipulation

--- src/lib/multer.ts
+++ src/lib/multer.ts
@@ -1,13 +1,21 @@
 import multer from "multer";
 
+const getUniqueFilename = (file: Express.Multer.File): string => {
+	const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+
+	const originalNameSplit = file.originalname.split(".");
+	const fileExtension = originalNameSplit.pop();
+	const originalNameWithoutFileExtension = originalNameSplit.join("");
+
+	return `${originalNameWithoutFileExtension} - ${uniqueSuffix}.${fileExtension}`;
+};
+
 const storage = multer.diskStorage({
 	destination: "./uploads",
-	filename: (_req, file, cb) => {
-		const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-		cb(null, `${file.originalname}-${uniqueSuffix}`);
+	filename: (_req, file, callback) => {
+		const uniqueFilename = getUniqueFilename(file);
+		callback(null, uniqueFilename);
 	},
 });
 
-const upload = multer({ storage });
-
-export { upload };
+export const upload = multer({ storage });
